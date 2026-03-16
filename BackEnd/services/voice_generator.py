6182dfa4b807c996f5514eb6d7fa7199539b9312
Abothula Rajesh <abothularajesh58@gmail.com>
2026-03-17 00:35:23 +0530

    Enhance create_voice function with text cleaning
    Added text preprocessing to remove special characters before generating voice.

--- BackEnd/services/voice_generator.py
+++ BackEnd/services/voice_generator.py
@@ -1,7 +1,8 @@
 from gtts import gTTS
 
 def create_voice(text, filename):
-
+    #removing the special characters...
+    text = re.sub(r'^\d+[\.\)\-\s]*:', '', text)
     tts = gTTS(text)
 
     path = f"outputs/audio/{filename}.mp3"
